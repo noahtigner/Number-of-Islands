1d9845b1d3f45ca7702d839e03a411fc5f3953dd
Noah Tigner <nzt@cs.uoregon.edu>
2020-12-01 16:30:35 -0800

    Cleaned things up a bit

--- analysis.py
+++ analysis.py
@@ -5,7 +5,13 @@ import rasterio
 from number_of_islands import NumberOfIslands
 
 def is_land_simple(r, g, b):
-    # return (r>=20 or g>=20 or b>=20)
+    """
+    simple classifier uses redness of a cell
+
+    args:   (r, g, b), the 3 band values for a given cell (need not be r, g, b)
+    effects: 
+    returns: boolean
+    """
     return r > 12
 
 def is_land_comparison(r, g, b):
@@ -25,13 +31,28 @@ def is_land_classifier(r, g, b):
     return True
 
 def is_land(r, g, b):
+    """
+    decide if a pixel fits the definition of land
+
+    args:   (r, g, b), the 3 band values for a given cell (need not be r, g, b)
+    effects: 
+    returns: boolean
+    """
+
     b1 = is_land_simple(r, g, b)
-    b2 = is_land_comparison(r, g, b) 
-    b3 = is_land_classifier(r, g, b)
-    # print(f"{(r, g, b)} -> {b1, b2, b3}")
+    # b2 = is_land_comparison(r, g, b) 
+    # b3 = is_land_classifier(r, g, b)
     return b1 # and b2 and b3
 
 def run(raster, algorithm, is_land, out, out_folder, prints=True):
+    """
+    a helper function for running the analysis
+
+    args:   raster, algorithm, is_land, out, out_folder, prints
+    effects: prints output (if prints=True), creates rasters (if out=True)
+    returns: (elapsed time, number of islands)
+    """
+
     start_time = time.perf_counter()
     noi = NumberOfIslands(raster, algorithm=algorithm, is_land=is_land, out=out, out_folder=out_folder)
     if prints:
@@ -48,11 +69,25 @@ def run(raster, algorithm, is_land, out, out_folder, prints=True):
     except:
         return (None, None)
 
+################
+
 filename = os.path.join(os.path.dirname(__file__), 'out')
 
+# list of input rasters
 rasters = ["in/nasa_blueMarble_240p.tiff", "in/nasa_blueMarble_480p.tiff", "in/nasa_blueMarble_720p.tiff"]
+
+# the three search algorithms implemented
 algorithms = ["dfs", "dfs'", "bfs"]
 
+# read in 3 bands from input raster
+with rasterio.open(rasters[3]) as dataset:
+    rs, gs, bs = dataset.read()[0:3]
+
+# simple example of module use
+run(raster=[rs, gs, bs], algorithm="bfs", is_land=is_land, out=False, out_folder=filename)
+
+# complec example of module use - for comparing algorithms
+# for each raster, print the elapsed time of each algorithm
 for raster in rasters:
     times = {algo: [] for algo in algorithms}
 
@@ -64,7 +99,3 @@ for raster in rasters:
             times[algo].append(e)
 
     print(f"{raster}:\t{times}")
-
-# with rasterio.open(rasters[3]) as dataset:
-#     rs, gs, bs = dataset.read()[0:3]
-# run(raster=[rs, gs, bs], algorithm="bfs", is_land=is_land, out=False, out_folder=filename)
